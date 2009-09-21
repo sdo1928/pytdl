bd40f06fe71d222482dcb042de21ab5bde3b1eb6
sdo1928 <scott@flywheelnetworks.com>
2009-09-21 13:18:33 -0700

    re-push of mod'ed files

--- newtdl.py
+++ newtdl.py
@@ -1,12 +1,11 @@
-import todolist_sub as todolist
-import mx.DateTime as DateTime
 import sys, os
+import mx.DateTime as DateTime
+import todolist_sub as todolist
 from todolist_sub import TODOLISTSub as TODOLIST
 from todolist_sub import TASKSub as TASK
 
-
-# attempt to match todolist.exe new file behavior
-#TODOLIST
+#attempt to match todolist.exe new file behavior
+#New TODOLIST
 newTodolist = TODOLIST()
 
 #newTodolist.set_CATEGORY
@@ -21,97 +20,15 @@ newTodolist.set_PROJECTNAME('')
 td = DateTime.today()
 newTodolist.set_LASTMODIFIED(td.strftime('%m/%d/%Y'))
 newTodolist.set_EARLIESTDUEDATE('0.0000000')
-newTodolist.set_NEXTUNIQUEID(2)
+newTodolist.set_NEXTUNIQUEID(1)
 newTodolist.set_FILEFORMAT(9)
 
-
-#TASK
+#New TASK
 newTask = TASK()
-PRIORITYCOLOR="15732480"
-PRIORITYWEBCOLOR="#000FF0"
-
-newTask.set_TITLE('Task')
-#newTask.set_ALLOCATEDBY
-newTask.set_CALCCOST('0.00000000')
-#newTask.set_CALCPERCENTDONE
-#newTask.set_CALCTIMEESTIMATE
-#newTask.set_CALCTIMESPENT
-#newTask.set_CATEGORY
-#newTask.set_CATEGORY1
-#newTask.set_CATEGORY2
-#newTask.set_CATEGORY3
-#newTask.set_CATEGORY4
-#newTask.set_CATEGORY5
-#newTask.set_CATEGORY6
-#newTask.set_CATEGORY7
-#newTask.set_CATEGORY8
-#newTask.set_CATEGORY9
-#newTask.set_COLOR
-#newTask.set_COMMENTS
-#newTask.set_COMMENTSTYPE
-newTask.set_COST('0.00000000')
-newTask.set_CREATIONDATE(td.COMDate())
-newTask.set_CREATIONDATESTRING(td.strftime('%m/%d/%Y'))
-#newTask.set_CUSTOMCOMMENTS
-#newTask.set_DEPENDS
-#newTask.set_DEPENDS1
-#newTask.set_DEPENDS2
-#newTask.set_DEPENDS3
-#newTask.set_DEPENDS4
-#newTask.set_DEPENDS5
-#newTask.set_DEPENDS6
-#newTask.set_DEPENDS7
-#newTask.set_DEPENDS8
-#newTask.set_DEPENDS9
-#newTask.set_DONEDATE
-#newTask.set_DONEDATESTRING
-#newTask.set_DUEDATE
-#newTask.set_DUEDATESTRING
-#newTask.set_EXTERNALID
-#newTask.set_FILEREFPATH
-#newTask.set_FLAG
-newTask.set_ICONINDEX(-1)
-newTask.set_ID(1)
-#newTask.set_LASTMOD
-#newTask.set_LASTMODSTRING
-#newTask.set_NUMCATEGORY
-#newTask.set_NUMDEPENDS
-#newTask.set_NUMPERSON
-newTask.set_PERCENTDONE(0)
-#newTask.set_PERSON
-#newTask.set_PERSON1
-#newTask.set_PERSON2
-#newTask.set_PERSON3
-#newTask.set_PERSON4
-#newTask.set_PERSON5
-#newTask.set_PERSON6
-#newTask.set_PERSON7
-#newTask.set_PERSON8
-#newTask.set_PERSON9
-newTask.set_POS(1)
-newTask.set_PRIORITY(5)
-newTask.set_PRIORITYCOLOR(PRIORITYCOLOR)
-newTask.set_PRIORITYWEBCOLOR(PRIORITYWEBCOLOR)
-newTask.set_RISK(0)
-newTask.set_STARTDATE(td.COMDate())
-newTask.set_STARTDATESTRING(td.strftime('%m/%d/%Y'))
-#newTask.set_STATUS
-#newTask.set_TEXTCOLOR
-#newTask.set_TEXTWEBCOLOR
-#newTask.set_TIMEESTIMATE
-#newTask.set_TIMEESTUNITS
-#newTask.set_TIMESPENT
-#newTask.set_TIMESPENTUNITS
-#newTask.set_TITLE
-#newTask.set_VERSION
-#newTask.set_WEBCOLOR
+newTask.set_TITLE('Sample Task')
 
 newTodolist.add_TASK(newTask)
 
-fileOut = open('tmp.tdl', 'w')
-
+fileOut = open('new.tdl', 'w')
 newTodolist.export(fileOut, 1, '')
-
 fileOut.close()
-
-
--- todolist_sub.py
+++ todolist_sub.py
@@ -8,6 +8,8 @@ import sys
 from string import lower as str_lower
 from xml.dom import minidom
 from xml.sax import handler, make_parser
+from xml.dom import Node
+import mx.DateTime as DateTime
 
 import todolist as supermod
 
@@ -24,15 +26,108 @@ ExternalEncoding = 'ascii'
 class TODOLISTSub(supermod.TODOLIST):
     def __init__(self, FILEVERSION=None, PROJECTNAME=None, FILENAME=None, CUSTOMCOMMENTSTYPE=None, LASTMODIFIED=None, EARLIESTDUEDATE=None, NEXTUNIQUEID=None, FILEFORMAT=None, TASK=None, CATEGORY=None, STATUS=None, PERSON=None, ALLOCATEDBY=None, VERSION=None):
         supermod.TODOLIST.__init__(self, FILEVERSION, PROJECTNAME, FILENAME, CUSTOMCOMMENTSTYPE, LASTMODIFIED, EARLIESTDUEDATE, NEXTUNIQUEID, FILEFORMAT, TASK, CATEGORY, STATUS, PERSON, ALLOCATEDBY, VERSION)
+    def add_TASK(self, value):
+        nextid = self.get_NEXTUNIQUEID()
+        if nextid is None:
+            nextid = 1
+        value.set_ID(nextid)
+        nextid = int(nextid) + 1
+        self.set_NEXTUNIQUEID(nextid)
+        maxpos = 0
+        for task in self.get_TASK():
+            if int(task.get_POS()) > maxpos:
+                maxpos = int(task.get_POS())
+        value.set_POS(maxpos + 1)
+        self.TASK.append(value)
 supermod.TODOLIST.subclass = TODOLISTSub
 # end class TODOLISTSub
 
 
 class TASKSub(supermod.TASK):
-    def __init__(self, CATEGORY1=None, STARTDATE=None, CATEGORY3=None, CATEGORY2=None, CATEGORY5=None, CATEGORY4=None, CATEGORY7=None, CATEGORY6=None, CATEGORY9=None, CATEGORY8=None, NUMPERSON=None, PERSON2=None, PERSON3=None, COST=None, EXTERNALID=None, DEPENDS9=None, PERSON4=None, PERSON5=None, DUEDATESTRING=None, ICONINDEX=None, CREATIONDATESTRING=None, PERSON9=None, WEBCOLOR=None, PERSON8=None, LASTMOD=None, TIMEESTUNITS=None, COLOR=None, TITLE=None, LASTMODSTRING=None, PRIORITYWEBCOLOR=None, POS=None, ID=None, PRIORITY=None, CALCTIMESPENT=None, DEPENDS=None, NUMCATEGORY=None, DONEDATESTRING=None, CREATIONDATE=None, PERCENTDONE=None, TIMEESTIMATE=None, STATUS=None, DEPENDS7=None, ALLOCATEDBY=None, DONEDATE=None, RISK=None, STARTDATESTRING=None, TEXTCOLOR=None, NUMDEPENDS=None, CALCPERCENTDONE=None, FLAG=None, VERSION=None, TIMESPENT=None, TEXTWEBCOLOR=None, PRIORITYCOLOR=None, FILEREFPATH=None, DUEDATE=None, CALCTIMEESTIMATE=None, CATEGORY=None, DEPENDS6=None, DEPENDS5=None, DEPENDS4=None, DEPENDS3=None, DEPENDS2=None, DEPENDS1=None, CUSTOMCOMMENTS=None, TIMESPENTUNITS=None, COMMENTS=None, DEPENDS8=None, PERSON=None, PERSON1=None, CALCCOST=None, PERSON6=None, COMMENTSTYPE=None, PERSON7=None, valueOf_=''):
+    def __init__(self, TASK=None, CATEGORY1=None, STARTDATE=None, CATEGORY3=None, CATEGORY2=None, CATEGORY5=None, CATEGORY4=None, CATEGORY7=None, CATEGORY6=None, CATEGORY9=None, CATEGORY8=None, NUMPERSON=None, PERSON2=None, PERSON3=None, COST=None, EXTERNALID=None, DEPENDS9=None, PERSON4=None, PERSON5=None, DUEDATESTRING=None, ICONINDEX=None, CREATIONDATESTRING=None, PERSON9=None, WEBCOLOR=None, PERSON8=None, LASTMOD=None, TIMEESTUNITS=None, COLOR=None, TITLE=None, LASTMODSTRING=None, PRIORITYWEBCOLOR=None, POS=None, ID=None, PRIORITY=None, CALCTIMESPENT=None, DEPENDS=None, NUMCATEGORY=None, DONEDATESTRING=None, CREATIONDATE=None, PERCENTDONE=None, TIMEESTIMATE=None, STATUS=None, DEPENDS7=None, ALLOCATEDBY=None, DONEDATE=None, RISK=None, STARTDATESTRING=None, TEXTCOLOR=None, NUMDEPENDS=None, CALCPERCENTDONE=None, FLAG=None, VERSION=None, TIMESPENT=None, TEXTWEBCOLOR=None, PRIORITYCOLOR=None, FILEREFPATH=None, DUEDATE=None, CALCTIMEESTIMATE=None, CATEGORY=None, DEPENDS6=None, DEPENDS5=None, DEPENDS4=None, DEPENDS3=None, DEPENDS2=None, DEPENDS1=None, CUSTOMCOMMENTS=None, TIMESPENTUNITS=None, COMMENTS=None, DEPENDS8=None, PERSON=None, PERSON1=None, CALCCOST=None, PERSON6=None, COMMENTSTYPE=None, PERSON7=None, valueOf_=''):
         supermod.TASK.__init__(self, CATEGORY1, STARTDATE, CATEGORY3, CATEGORY2, CATEGORY5, CATEGORY4, CATEGORY7, CATEGORY6, CATEGORY9, CATEGORY8, NUMPERSON, PERSON2, PERSON3, COST, EXTERNALID, DEPENDS9, PERSON4, PERSON5, DUEDATESTRING, ICONINDEX, CREATIONDATESTRING, PERSON9, WEBCOLOR, PERSON8, LASTMOD, TIMEESTUNITS, COLOR, TITLE, LASTMODSTRING, PRIORITYWEBCOLOR, POS, ID, PRIORITY, CALCTIMESPENT, DEPENDS, NUMCATEGORY, DONEDATESTRING, CREATIONDATE, PERCENTDONE, TIMEESTIMATE, STATUS, DEPENDS7, ALLOCATEDBY, DONEDATE, RISK, STARTDATESTRING, TEXTCOLOR, NUMDEPENDS, CALCPERCENTDONE, FLAG, VERSION, TIMESPENT, TEXTWEBCOLOR, PRIORITYCOLOR, FILEREFPATH, DUEDATE, CALCTIMEESTIMATE, CATEGORY, DEPENDS6, DEPENDS5, DEPENDS4, DEPENDS3, DEPENDS2, DEPENDS1, CUSTOMCOMMENTS, TIMESPENTUNITS, COMMENTS, DEPENDS8, PERSON, PERSON1, CALCCOST, PERSON6, COMMENTSTYPE, PERSON7, valueOf_)
+        if TASK is None:
+            self.TASK = []
+        else:
+            self.TASK = TASK
+        if PRIORITYCOLOR is None or PRIORITYWEBCOLOR is None:
+            self.PRIORITYCOLOR = "15732480"
+            self.PRIORITYWEBCOLOR = "#000FF0"
+        else:
+            self.PRIORITYCOLOR = PRIORITYCOLOR
+            self.PRIORITYWEBCOLOR = PRIORITYWEBCOLOR
+        if COST is None:
+            self.COST = '0.00000000'
+        else:
+            self.COST = COST
+        if CREATIONDATE is None or CREATIONDATESTRING is None:
+            td = DateTime.today()
+            self.CREATIONDATE = td.COMDate()
+            self.CREATIONDATESTRING = td.strftime('%m/%d/%Y')
+        else:
+            self.CREATIONDATE = CREATIONDATE
+            self.CREATIONDATESTRING = CREATIONDATESTRING
+        if CALCCOST is None:
+            self.CALCCOST = '0.00000000'
+        else:
+            self.CALCCOST = CALCCOST
+        if ICONINDEX is None:
+            self.ICONINDEX = -1
+        else:
+            self.ICONINDEX = ICONINDEX
+        if PERCENTDONE is None:
+            self.PERCENTDONE = 0
+        else:
+            self.PERCENTDONE = PERCENTDONE
+        if PRIORITY is None:
+            self.PRIORITY = 5
+        else:
+            self.PRIORITY = PRIORITY
+        if RISK is None:
+            self.RISK = 0
+        else:
+            self.RISK = RISK
+        if STARTDATE is None or sTARTDATESTRING is None:
+            self.STARTDATE = self.CREATIONDATE
+            self.STARTDATESTRING = self.CREATIONDATESTRING
+        else:
+            self.STARTDATE = STARTDATE
+            self.STARTDATESTRING = STARTDATESTRING
+    def buildChildren(self, child_, nodeName_):
+        if child_.nodeType == Node.ELEMENT_NODE and \
+            nodeName_ == 'TASK':
+            obj_ = supermod.TASK.factory()
+            obj_.build(child_)
+            self.TASK.append(obj_)
+        elif child_.nodeType == Node.TEXT_NODE:
+            self.valueOf_ += child_.nodeValue
+        elif child_.nodeType == Node.CDATA_SECTION_NODE:
+            self.valueOf_ += '![CDATA['+child_.nodeValue+']]'
+    def get_TASK(self): return self.TASK
+    def set_TASK(self, TASK): self.TASK = TASK
+    def add_TASK(self, value): self.TASK.append(value)
+    def insert_TASK(self, index, value): self.TASK[index] = value
+    def exportChildren(self, outfile, level, namespace_='', name_='TASK'):
+        for TASK_ in self.TASK:
+            TASK_.export(outfile, level, namespace_, name_='TASK')
+        if self.valueOf_.find('![CDATA')>-1:
+            value=supermod.quote_xml('%s' % self.valueOf_)
+            value=value.replace('![CDATA','<![CDATA')
+            value=value.replace(']]',']]>')
+            outfile.write(value)
+        else:
+            outfile.write(supermod.quote_xml('%s' % self.valueOf_))
+    def hasContent_(self):
+        if (
+            self.TASK or
+            self.valueOf_
+            ):
+            return True
+        else:
+            return False
 supermod.TASK.subclass = TASKSub
 # end class TASKSub
+
 
 
 class CATEGORYSub(supermod.CATEGORY):
